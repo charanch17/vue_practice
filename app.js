6ba6b10cb28b640dd24f61bdd55aac8242e1df66
charanch17 <ch.s.r.acharyulu17@gmail.com>
2021-04-01 21:10:31 +0530

    done with data binding i.e v-html and v-bind

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 new Vue({
-    el:".baby",
-    data:{"name":"babu"},
-    methods:{
-        greet:function(times){
-            return 'good '+times+' '+this.name;
-             }
-    }
-})
+  el: ".baby",
+  data: { name: "babu" ,
+    website:"https://vuejs.org/v2/guide/instance.html#Data-and-Methods",
+    website_tag:'<a href="https://www.google.com/">google</a>',
+    
+},
+  methods: {
+    greet: function (times) {
+      return "good " + times + " " + this.name;
+    },
+  },
+});
