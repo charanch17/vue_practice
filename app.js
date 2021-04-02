3e8ea5c4796c19600e72dcbd25f33bf01817dcc8
charanch17 <ch.s.r.acharyulu17@gmail.com>
2021-04-02 10:28:49 +0530

    done with basic template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 new Vue({
-  el: ".baby",
-  data: { name: "babu" ,
+  el: ".ba",
+  data: { name: "bh" ,
     website:"https://vuejs.org/v2/guide/instance.html#Data-and-Methods",
     website_tag:'<a href="https://www.google.com/">google</a>',
     
